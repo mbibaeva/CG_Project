1489e97cf9e3fc5618bb05e2b2eeb3368a586a8b
mbibaeva <melisanushk@gmail.com>
2017-12-21 15:27:48 +0300

    Add files via upload

---
+++ super_code.R
@@ -0,0 +1,19 @@
+df1 <- read.csv('all_data.csv', sep=';')
+df1
+fit <- glm(Target ~ Number+Gender+Animacy+Verb_of_speech+Ger+Verb_time+Object+Protiv1+Protiv2, data = df1, family = "binomial") 
+summary(fit)
+
+glm(formula = Target ~ Number + Gender + Animacy + Verb_of_speech + Ger + Verb_time + Object + Protiv1 + Protiv2, family = "binomial", data = df1)
+
+#график
+
+install.packages("corrplot")
+library('corrplot')
+loaddata <- read.csv('/Users/Kartozianstvo/Desktop/вышечка/cxg_2017/encode.csv', sep=',')
+df = subset(loaddata, select = -c(X) )
+corrmatr <- cor(df)
+res1 <- cor.mtest(df, conf.level = .99)
+corrplot(corrmatr, p.mat = res1$p, insig = "blank")
+
+
+#chisq.test(loaddata$Target, loaddata$Protiv1, correct=FALSE)
